cdb4ac976b8694f9d9961286572ea39223981085
Mu-ik Jeon <muikor@gmail.com>
2012-05-18 20:35:10 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sayho::Application.config.secret_token = '8a68c17c712ed6a59668227aab91c5bcf578c4fd4b7fc60a5a8fddad9e40076f25f8b8a038b59a9aebbf7725f6ff3873e7d0725b979bbb1a38b3c633f8347d88'
